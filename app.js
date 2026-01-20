5125bf8d41e7ee37511eea773d744c3518bf9d8b
afrin shaik <skafrin7013@gmail.com>
2026-01-20 22:30:19 +0530

    Variable Learned

---
+++ app.js
@@ -0,0 +1,11 @@
+// window.alert("Welcome to new beginnings!");
+// alert("Welcome to afrin beginnings!");
+let a=10;
+let b=10;
+if(a==b)
+{
+  alert("Welcome to afrin beginnings!");
+}
+else{
+  window.alert("I'm here");
+}
