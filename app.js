363ede94d392f135ccd8cb561b90933dd11cfdaf
afrin shaik <skafrin7013@gmail.com>
2026-01-20 23:10:24 +0530

    Variable's Learned

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 // window.alert("Welcome to new beginnings!");
 // alert("Welcome to afrin beginnings!");
-let a=10;
-let b=10;
-if(a==b)
-{
-  alert("Welcome to afrin beginnings!");
-}
-else{
-  window.alert("I'm here");
-}
+age=23;
+var name="Afrin";
+console.log(age);
+// let a=10;
+// let b=10;
+// if(a==b)
+// {
+//   alert("Welcome to afrin beginnings!");
+// }
+// else{
+//   window.alert("I'm here");
+// }
